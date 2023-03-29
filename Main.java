df9b5b453388c81cf75c0c95a2bc630d6a2d30bb
BielasR <126687810+BielasR@users.noreply.github.com>
2023-03-29 13:04:22 +0000

    okienko

--- Main.java
+++ Main.java
@@ -10,19 +10,33 @@ public class Main
         System.out.println("Podaj symbol");
         String s = symbol.nextLine();
     
-        System.out.println("Podaj wysokosc choinki");
+        System.out.println("Podaj ilość boku");
         int w = wysokość.nextInt();
   
   
-        for (int i = 1; i<=w; i++)
+        for (int j = 0; j<w; j++)
         {
-            for(int j=0;j<w-i;j++)
-              System.out.print(" ");
-          
-            for (int j = 0; j<(i*2)-1; j++) //ustala ilość symboli w linijce 
+            for (int z = 0; z<w; z++)
               System.out.print(s);
               System.out.println(" ");
         }
+      System.out.println(" ");
+
+      String s2 = "*";
   
+      for (int i = 1; i<=4; i++)
+        {
+            for (int a = 1; a<=4; a++)
+           { 
+              if(i>1 && i<4 && a>1 && a<4)
+              System.out.print(" ");
+             else
+                System.out.print(s2);
+                 
+            }
+         System.out.println(" ");
+        }
+     
     }
 }
+  
